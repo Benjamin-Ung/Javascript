591b408c8ae201b3307f75f1ef361476c974e321
Benjamin Ung <Benjaminung@cpp.edu>
2022-05-02 16:00:30 -0700

    trying to keep things synced up...

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
 var number = 5; // in line comment
+/* mult-line comments: following tutorial: https://www.youtube.com/watch?v=PkZNo7MFNFg
+*/
+
+//data types:
+//undefined, null, boolean, string, symbol, number, and object
+
+var myName = "Ben";
+myName = 8;
+let ourName = "FreeCodeCamp";
+const pi = 3.14
+
+console.log(pi);
